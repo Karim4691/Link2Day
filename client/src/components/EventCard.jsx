d736defa7e2323431b09ed636c9806049fa39572
Karim4691 <kalami2003@gmail.com>
2025-08-11 15:13:12 -0400

    Minor fixes

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -5,8 +5,8 @@ export default function EventCard({ event }) {
 
   return (
     <Link to={`/events/${event._id}`}>
-      <div className="text-left flex flex-row items-start cursor-pointer max-h-48 pr-2 mb-6 w-fit">
-        <img src={event.photoUrl} alt="Event" className='h-44 mr-2 p-2 rounded-xl w-32 md:w-64' />
+      <div className="text-left flex flex-row items-start cursor-pointer h-36 pr-2 mb-6 w-fit">
+        <img src={event.photoUrl} alt="Event" className='h-full mr-2 p-2 rounded-xl w-32 md:w-64' />
         <div className='flex flex-col h-full justify-evenly ml-2'>
           <h3 className='text-lg text-[#C19A6B] line-clamp-2 w-36 md:w-72'>{utcTimestampToLocal(event.eventStart, event.timeZoneId)}</h3>
           <p className='font-semibold text-lg line-clamp-2 w-36 md:w-72'>{event.title}</p>
